1f804312967203a8674cdef6fe440e2e8f6a42ad
valemancilla <217262158+valemancilla@users.noreply.github.com>
2025-08-04 14:46:47 -0500

    feat: :sparkles: examen
    examen numero 1

---
+++ app/main.py
@@ -0,0 +1,9 @@
+
+
+def main_menu():
+    while True:
+        print("=====================")
+        print("selecciona la opncion")
+        print("=======================")
+        print("1. ver ingredientes")
+        print("")
