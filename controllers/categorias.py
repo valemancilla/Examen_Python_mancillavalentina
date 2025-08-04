bb3b4461c03e7a2ca87796654135ae7d70cbcacf
valemancilla <217262158+valemancilla@users.noreply.github.com>
2025-08-04 17:31:37 -0500

    feat: :sparkles: listo
    se realizo el ultimo cambio

--- controllers/categorias.py
+++ controllers/categorias.py
@@ -1,49 +1,47 @@
 import json
-categorias_file="categorias.json"
+categorias_file = "categorias.json"
 
 def cargar_categoria():
     try:
-        with open (categorias_file,"r")as file:
+        with open(categorias_file, "r") as file:
             return json.load(file)
     except FileNotFoundError:
-        return[]
-    
-def guardar_categoria(data):
-    with open(categorias_file,"w") as file:
-        json.dump(data,file,indent=4)
+        return []
 
+def guardar_categoria(data):
+    with open(categorias_file, "w") as file:
+        json.dump(data, file, indent=4)
 
 def agregar_categoria():
-    categorias=cargar_categoria()
-    nombre=input("nombre de la categoria")
-    descripcion=input("descripcion:")
+    categorias = cargar_categoria()
+    nombre = input("Nombre de la categoria: ")
+    descripcion = input("Descripción: ")
     categorias.append({
-        "nombre":nombre,
-        "descripcion":descripcion
-
+        "nombre": nombre,
+        "descripcion": descripcion
     })
 
     guardar_categoria(categorias)
-    print("categoria guardad")
+    print("Categoría guardada")
 
 def ver_categoria():
-    categoria=cargar_categoria()
+    categoria = cargar_categoria()
     for cat in categoria:
-        print(f'{cat['nombre']}-{cat['descripcion']}')
-
-def actualizar_categoria():
-    categoria=categorias_file()
-    nombre=input("nombre de la categoriaa actualizar:")
-    for cat in categoria: 
-        if cat ["nombre"].lower()== nombre.lower():
-            cat["descripcion"]=input("nueva descripcion:")
-            guardar_categoria(categoria)
-            print("categoria actualizada")
-            return
+        print(f'{cat["nombre"]} - {cat["descripcion"]}')
         
+def actualizar_categoria():
+    categorias = cargar_categoria()  
+    nombre = input("Nombre de la categoría a actualizar: ")
+    for cat in categorias: 
+        if cat["nombre"].lower() == nombre.lower():
+            cat["descripcion"] = input("Nueva descripción: ")
+            guardar_categoria(categorias)
+            print("Categoría actualizada")
+            return
+    print("Categoría no encontrada.")  
 def eliminar_categoria():
-    categorias=cargar_categoria()
-    nombre=input("nombre de la categoria a eliminar:")
-    categorias=[cat for cat in categorias if cat ["nombre"].lower()!=nombre.lower()]
+    categorias = cargar_categoria()
+    nombre = input("Nombre de la categoría a eliminar: ")
+    categorias = [cat for cat in categorias if cat["nombre"].lower() != nombre.lower()]
     guardar_categoria(categorias)
-    print("categoria eliminada")
+    print("Categoría eliminada")
